0948e5963e87d2509e8947abded9567590d02e62
Nipton <Nipton@NIPTON>
2024-07-09 13:45:12 +0400

    Добавьте файлы проекта.

---
+++ Client/Chat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Chat
+    {
+        readonly IPEndPoint localEndPoint;
+        readonly IPEndPoint remoteEndPoint;
+        Message message = new Message();
+        public Chat(IPEndPoint localEndPoint, IPEndPoint remoteEndPoint)
+        {
+            this.localEndPoint = localEndPoint;
+            this.remoteEndPoint = remoteEndPoint;
+        }
+        
+        public async Task SendMessageAsync()
+        {
+            Console.WriteLine("Введите имя:");
+            message.Name = Console.ReadLine()!;
+            using UdpClient udpClient = new();
+            Console.WriteLine("Введите сообщение:");
+            while (true)
+            {
+                try
+                {
+                    message.Text = Console.ReadLine();
+                    message.Date = DateTime.Now;
+                    string messegeToSend = message.ToJson();
+                    byte[] data = Encoding.UTF8.GetBytes(messegeToSend);
+                    await udpClient.SendAsync(data, remoteEndPoint);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public async Task ReceiveMessageAsync()
+        {
+            using UdpClient udpClient = new(localEndPoint);
+            while (true)
+            {
+                try
+                {
+                    var result = await udpClient.ReceiveAsync();
+                    var message = Message.FromJson(Encoding.UTF8.GetString(result.Buffer));
+                    Console.WriteLine(message);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }           
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Введите локальный порт: ");
+            if (!ushort.TryParse(Console.ReadLine(), out var localPort))
+            {
+                Console.WriteLine("Неверный ввод.");
+                return;
+            }
+            Console.Write("Введите удаленный порт: ");
+            if (!ushort.TryParse(Console.ReadLine(), out var remotePort))
+            {
+                Console.WriteLine("Неверный ввод.");
+                return;
+            }
+            Console.Clear();
+            IPEndPoint localPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), localPort);
+            IPEndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), remotePort);
+            Chat chat = new Chat(localPoint, remotePoint);
+            try
+            {
+                Task.Run(chat.ReceiveMessageAsync);
+                await chat.SendMessageAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
